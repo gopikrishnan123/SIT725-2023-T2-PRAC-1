4ecceee09462e38a9af9f305d93c02759b72d0e0
GOPIKRISHNAN B <gopikrishnan314@gmail.com>
2023-07-26 01:26:10 +1000

    website to change text and demo

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeText(){
+    var potentialText = ["text 1","text 2", "text 3", "text 4", "text 5"]
+    var num = getRandomNumber(0,potentialText.length - 1)
+    console.log("Index: ", num)
+    document.getElementById("text").innerHTML = potentialText[num];
+    }
+
+    function getRandomNumber(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+    }
+    
